f382ad9e8badccdff2bedf70f75a484e14fa773d
Fritz <friedrichgrabner09@gmail.com>
2024-09-09 19:20:10 +0200

    KI Output working, UI changes

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
-#--imports--
+import threading
 from tkinter import *
 from tkinter import ttk
 from pgpt_python.client import PrivateGPTApi
-import PyPDF2
 import os
 import re
+import PyPDF2
+
+pdf = 'pdf/20182.pdf'
+output_dir = 'txt/'
+
+# -----------------------------------------------------------------------------------------------------------------------
 
 client = PrivateGPTApi(base_url="http://localhost:8001")
+muster = r'\(A\)|\(B\)|\(C\)|\(D\)|Gesamtherstellung:.*?\-8333|\(.*?\)'
+
+# -----------------------------------------------------------------------------------------------------------------------
+
+woerter = []
 
 def check_client_health():
     if client.health.health().status == 'ok':
@@ -15,60 +25,13 @@ def check_client_health():
         print("pGPT ist nicht startbereit, bitte überprüfe den Server! Das Programm wurde beendet.")
         os._exit(0)
 
+def insert_text(text):
+    output_text.config(state='normal')
+    output_text.insert(END, "AI:    " + text)
+    output_text.insert(END, '\n' + "--------------------------------------------------" + '\n')
+    output_text.config(state='disabled')
+    woerter.clear()
 
-print(client.health.health().status)
-
-check_client_health()
-
-#-----------------------------------------------------------------------------------------------------------------------
-
-#--pdf to txt | Kürzung des Textes--
-
-#--Variablen--
-
-pdf = 'pdf/20182.pdf'
-muster = r'\(A\)|\(B\)|\(C\)|\(D\)|Gesamtherstellung:.*?\-8333|\(.*?\)'
-output_dir = 'txt/'
-
-
-def remove_after_period(s):
-    s = s.split('.')[0]
-    s = s.split('/')[1]
-    return s
-
-
-result = remove_after_period(pdf)
-
-
-def get_txt(input_pdf, output_dir):
-    with open(input_pdf, 'rb') as pdf_file:
-        reader = PyPDF2.PdfReader(pdf_file)
-        text = ''
-        for page in reader.pages:
-            text += page.extract_text() + '\n'
-            ergebnis = re.sub(muster, '', text, flags=re.DOTALL)
-
-    # Text in eine .txt-Datei schreiben
-    output_txt = os.path.join(output_dir, f'{result}.txt')
-    with open(output_txt, 'w', encoding='utf-8') as txt_file:
-        txt_file.write(ergebnis)
-
-get_txt(f'pdf/{result}.pdf', output_dir)
-
-print(f"Text wurde in '{os.path.join(output_dir, f'{result}.txt')}' geschrieben.")
-
-#-----------------------------------------------------------------------------------------------------------------------
-
-# --Fenster Einstellungen--
-root = Tk()
-root.title("Bundestagsrede Abfrage")
-root.geometry("800x600")
-root.resizable(False, False)
-root.configure(bg='white')
-
-#-----------------------------------------------------------------------------------------------------------------------
-
-# --Tk | Funktionen--
 def on_enter(e):
     e.widget['background'] = 'lightblue'
 
@@ -76,8 +39,19 @@ def on_leave(e):
     e.widget['background'] = 'SystemButtonFace'
 
 def print_input(event):
-    print(entry_input.get())
-    entry_input.delete(0, END)
+    if not request_in_progress.get():
+        entry = entry_input.get()
+        entry_input.config(state='disabled', disabledbackground='lightgrey')
+        request_in_progress.set(True)
+        threading.Thread(target=send_input, args=(entry,)).start()
+        entry_input.delete(0, END)
+
+def send_input(entry):
+    for i in client.contextual_completions.prompt_completion_stream(prompt=entry):
+        woerter.append(i.choices[0].delta.content)
+    entry_input.config(state='normal')
+    request_in_progress.set(False)
+    insert_text(' '.join(woerter))
 
 def add_placeholder(event, text):
     if entry_input.get() == '':
@@ -89,9 +63,40 @@ def remove_placeholder(event, text):
         entry_input.delete(0, END)
         entry_input.config(foreground='black')
 
-#-----------------------------------------------------------------------------------------------------------------------
+def remove_after_period(s):
+    s = s.split('.')[0]
+    s = s.split('/')[1]
+    return s
 
-# --Quit Button Frame--
+result = remove_after_period(pdf)
+
+def get_txt(input_pdf, output_dir):
+    with open(input_pdf, 'rb') as pdf_file:
+        reader = PyPDF2.PdfReader(pdf_file)
+        text = ''
+        for page in reader.pages:
+            text += page.extract_text() + '\n'
+            ergebnis = re.sub(muster, '', text, flags=re.DOTALL)
+
+    output_txt = os.path.join(output_dir, f'{result}.txt')
+    with open(output_txt, 'w', encoding='utf-8') as txt_file:
+        txt_file.write(ergebnis)
+
+get_txt(f'pdf/{result}.pdf', output_dir)
+print(f"Text wurde in '{os.path.join(output_dir, f'{result}.txt')}' geschrieben.")
+
+check_client_health()
+
+# -----------------------------------------------------------------------------------------------------------------------
+
+root = Tk()
+root.title("Bundestagsrede Abfrage")
+root.geometry("800x600")
+root.resizable(False, False)
+root.configure(bg='white')
+
+request_in_progress = BooleanVar(value=False)
+
 frm_quit_btn = Frame(root, bg='lightblue')
 frm_quit_btn.pack(side=BOTTOM, anchor='w', padx=10, pady=10)
 
@@ -103,59 +108,51 @@ quit_button.configure(bg='white')
 quit_button.bind("<Enter>", on_enter)
 quit_button.bind("<Leave>", on_leave)
 
-#-----------------------------------------------------------------------------------------------------------------------
-
-# --Frage stellen Button--
 placeholder = 'Stelle eine Frage...'
 
 frm_question = ttk.Frame(root)
 frm_question.pack(anchor=CENTER, side=BOTTOM)
 
-
-
-# --Variablen für Eingabefeld Größe--
 entry_width_input = 400
 entry_height_input = 50
 canvas_width_input = entry_width_input + 10
 canvas_height_input = entry_height_input + 10
 
-# --Canvas für abgerundete Ecken--
-canvas_input = Canvas(frm_question, width=canvas_width_input, height=canvas_height_input, bg='white', highlightthickness=0)
+canvas_input = Canvas(frm_question, width=canvas_width_input, height=canvas_height_input, bg='white',
+                      highlightthickness=0)
 canvas_input.pack()
 
-# --Abgerundetes Rechteck zeichnen--
 radius = 15
-canvas_input.create_arc((0, 0, radius*2, radius*2), start=90, extent=90, fill='lightgrey', outline='lightgrey')
-canvas_input.create_arc((canvas_width_input-radius*2, 0, canvas_width_input, radius*2), start=0, extent=90, fill='lightgrey', outline='lightgrey')
-canvas_input.create_arc((0, canvas_height_input-radius*2, radius*2, canvas_height_input), start=180, extent=90, fill='lightgrey', outline='lightgrey')
-canvas_input.create_arc((canvas_width_input-radius*2, canvas_height_input-radius*2, canvas_width_input, canvas_height_input), start=270, extent=90, fill='lightgrey', outline='lightgrey')
-canvas_input.create_rectangle((radius, 0, canvas_width_input-radius, canvas_height_input), fill='lightgrey', outline='lightgrey')
-canvas_input.create_rectangle((0, radius, canvas_width_input, canvas_height_input-radius), fill='lightgrey', outline='lightgrey')
+canvas_input.create_arc((0, 0, radius * 2, radius * 2), start=90, extent=90, fill='lightgrey', outline='lightgrey')
+canvas_input.create_arc((canvas_width_input - radius * 2, 0, canvas_width_input, radius * 2), start=0, extent=90,
+                        fill='lightgrey', outline='lightgrey')
+canvas_input.create_arc((0, canvas_height_input - radius * 2, radius * 2, canvas_height_input), start=180, extent=90,
+                        fill='lightgrey', outline='lightgrey')
+canvas_input.create_arc(
+    (canvas_width_input - radius * 2, canvas_height_input - radius * 2, canvas_width_input, canvas_height_input),
+    start=270, extent=90, fill='lightgrey', outline='lightgrey')
+canvas_input.create_rectangle((radius, 0, canvas_width_input - radius, canvas_height_input), fill='lightgrey',
+                              outline='lightgrey')
+canvas_input.create_rectangle((0, radius, canvas_width_input, canvas_height_input - radius), fill='lightgrey',
+                              outline='lightgrey')
 
-# --Entry Widget auf Canvas platzieren--
 entry_input = Entry(frm_question, foreground='black', background='lightgrey', highlightthickness=0, borderwidth=0)
 entry_input.insert(0, placeholder)
 
-canvas_input.create_window(canvas_width_input//2, canvas_height_input//2, window=entry_input, width=entry_width_input, height=entry_height_input)
+canvas_input.create_window(canvas_width_input // 2, canvas_height_input // 2, window=entry_input,
+                           width=entry_width_input, height=entry_height_input)
 
-# --Bindings für Frage stellen Button--
 entry_input.bind("<FocusOut>", lambda event: add_placeholder(event, placeholder))
 entry_input.bind("<FocusIn>", lambda event: remove_placeholder(event, placeholder))
 entry_input.bind("<Return>", print_input)
 
-#-----------------------------------------------------------------------------------------------------------------------
-
-# --Output Fenster--
 frm_output = ttk.Frame(root)
-frm_output.pack(anchor=CENTER, side='top', pady=(100, 20))
+frm_output.pack(anchor=CENTER, side='top', pady=(20, 20), padx=10)
 
-# --Variablen für Ausgabe Größe--
-canvas_width_output = 600
-canvas_height_output = 400
+output_text = Text(frm_output, wrap='word', bg='lightgrey', fg='black', width=500, height=500, font=('Arial', 12))
+output_text.config(state='disabled')
+output_text.pack()
 
-canvas_output = Canvas(frm_output, width=canvas_width_output, height=canvas_height_output, bg='lightgrey', highlightthickness=5)
-canvas_output.pack()
-
-#-----------------------------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------------------------------------------
 
 root.mainloop()
